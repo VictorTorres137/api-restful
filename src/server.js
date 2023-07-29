efcb18ac1bf4d48950dcbe302c02c5c7309706dc
Victor Torres <vdst2011@hotmail.com>
2023-07-29 12:32:48 -0300

    Configurando CORS

--- src/server.js
+++ src/server.js
@@ -1,25 +1,42 @@
 const express = require('express')
-// const path = require('path')
+const cors = require('cors')
 
 const routes = require('./routes/routes')
 const db = require('./database/db')
-const internal = require('stream')
+
 
 const app = express()
-
 
 
 //conecção com banco de dados
 db.connect()
 
+const allowedOrigins = [
+    'http://127.0.0.1:5501',
+    "http://outraurl.com.br",
+]
+
+// habilita CORS
+app.use(cors({
+    origin: function(origin, callback) {
+        let allowed = true
+
+        console.log(origin)
+        console.log(!allowedOrigins.includes(origin))
+
+        //mobile app
+        if(!origin) allowed = true
+
+        if(!allowedOrigins.includes(origin)) allowed = false
+
+        console.log(allowed)
+
+        callback(null, allowed)
+    }
+}))
+
 //habilita server para receber dados em Json
 app.use(express.json())
-
-
-//definindo template engine
-
-//habilita server para receber dados via post (Formulário)
-// app.use(express.urlencoded({ extended: true}))
 
 
 //definindo rotas
